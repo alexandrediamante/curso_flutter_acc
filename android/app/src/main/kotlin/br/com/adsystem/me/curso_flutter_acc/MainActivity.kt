1ae7ce100cfa37c818a5ec9d5c0b794ea6391822
alexandrediamante <alexandrediamante@gmail.com>
2023-11-06 12:46:48 -0300

    Primeiro Commit

---
+++ android/app/src/main/kotlin/br/com/adsystem/me/curso_flutter_acc/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.adsystem.me.curso_flutter_acc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MeuApp());
+}
+
+class MeuApp extends StatelessWidget {
+  const MeuApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, //Removendo o marcador "Debug" --
+      home: Container(
+        decoration: const BoxDecoration(),
+        child: const MeuCorpo(),
+      ),
+    );
+  }
+}
+
+class MeuCorpo extends StatefulWidget {
+  const MeuCorpo({Key? key}) : super(key: key);
+
+  @override
+  MeuCorpoState createState() => MeuCorpoState();
+}
+
+class MeuCorpoState extends State<MeuCorpo> {
+  int contador = 0;
+
+  void incrementar() {
+    setState(() {
+      if (contador < 20) {
+        contador++;
+      }
+      if (contador == 20) {
+        _atualizarTexto('Lotado');
+      }
+    });
+  }
+
+  void decrementar() {
+    setState(() {
+      if (contador > 0) {
+        contador--;
+      }
+      if (contador <= 0) {
+        _atualizarTexto('Valor não pode ser menor que Zero, Verifique!');
+      }
+    });
+  }
+
+  void _atualizarTexto(String texto) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(texto), //
+        duration: const Duration(seconds: 1),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Contador Entrada'),
+        centerTitle: true,
+        backgroundColor: Colors.yellow,
+        titleTextStyle: TextStyle(fontSize: 23, color: Colors.blueAccent),
+      ),
+      backgroundColor: Colors.black,
+      body: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+          image: AssetImage('assets/images/Flutter01.png'),
+          fit: BoxFit.cover,
+        )),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  Text(
+                    contador >= 20 ? 'Lotado' : 'Pode Entrar',
+                    style: TextStyle(
+                      fontSize: 45,
+                      color: contador >= 20
+                          ? Colors.red
+                          : Colors.white, // Altera a cor dinamicamente
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  Text(
+                    '$contador',
+                    style: const TextStyle(
+                      fontSize: 50,
+                      color: Colors.white,
+                    ),
+                    textAlign: TextAlign.left,
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      decrementar();
+                    },
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.yellow,
+                      fixedSize: const Size(100, 100),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(24)),
+                    ),
+                    child: const Text(
+                      'Saiu',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      incrementar();
+                    },
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.yellow,
+                      fixedSize: const Size(100, 100),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(24)),
+                    ),
+                    child: const Text(
+                      'Entrou',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
